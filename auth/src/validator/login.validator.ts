19a529da68cf69a33a8ec3cc9e269bde194bfab9
Rounak00 <mukherjeerounak10@gmail.com>
2025-04-19 13:29:46 +0530

    Added the validation

--- auth/src/validator/login.validator.ts
+++ auth/src/validator/login.validator.ts
@@ -1,6 +1,16 @@
 import Joi from "joi";
 
 export const loginValidator = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
+   email: Joi.string().email().required().messages({
+        "string.base": "Email must be a string.",
+        "string.email": "Please enter a valid email address.",
+        "string.empty": "Email is required.",
+        "any.required": "Email is required."
+      }),
+  password: Joi.string().min(6).required().messages({
+        "string.base": "Password must be a string.",
+        "string.empty": "Password is required.",
+        "string.min": "Password must be at least 6 characters long.",
+        "any.required": "Password is required."
+      }),
 });
