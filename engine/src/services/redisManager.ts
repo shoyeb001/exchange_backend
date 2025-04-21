3e35a60df4284aca70bb6f7d23bcd100fbe6a2ef
shoyeb001 <shoyebjio3398@gmail.com>
2025-04-21 17:24:12 +0530

    docker containerize added

--- engine/src/services/redisManager.ts
+++ engine/src/services/redisManager.ts
@@ -5,9 +5,16 @@ export class RedisManager {
     private static instance: RedisManager;
 
     constructor() {
-        this.publisher = createClient();
+        this.publisher = createClient({
+            url: 'redis://127.0.0.1:6379'
+        }
+        );
         this.publisher.connect();
-        this.subscriber = createClient();
+        this.subscriber = createClient(
+            {
+                url: 'redis://127.0.0.1:6379'
+            }
+        );
         this.subscriber.connect();
     }
     public static getInstance() {
