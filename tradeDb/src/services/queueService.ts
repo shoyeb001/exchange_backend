b740e46d6f9baebd96dde95c01b23402e481bd07
shoyeb001 <shoyebjio3398@gmail.com>
2025-04-29 20:25:49 +0530

    order create workflow done
    update some issues in the tradeDb to align database

--- tradeDb/src/services/queueService.ts
+++ tradeDb/src/services/queueService.ts
@@ -6,9 +6,11 @@ import { RedisManager } from "./redisManager"
 export const startQueueService = async () => {
     while (true) {
         const response = await RedisManager.getInstance().popMessageFromQueue("db_processor");
+
         if (!response) {
 
         } else {
+            console.log(response)
             const data = JSON.parse(response as string);
             switch (data.type) {
                 case "TRADE_ADDED":
@@ -20,6 +22,7 @@ export const startQueueService = async () => {
                         quoteQuantity: data.payload.quoteQuantity,
                         tradeId: data.payload.tradeId
                     });
+                    break;
                 case "ADD_ORDER":
                     await db.insert(order).values({
                         userId: data.payload.userId,
@@ -32,11 +35,23 @@ export const startQueueService = async () => {
                         clientOrderId: data.payload.clientOrderId,
                         status: data.payload.status
                     });
+                    break;
                 case "UPDATE_ORDER":
                     const orderData = await db.query.order.findFirst({
                         where: and(eq(order.userId, data.payload.userId), eq(order.clientOrderId, data.payload.clientOrderId))
                     });
+                    const newFill = parseFloat(orderData?.executedQuantity.toString()!) + parseFloat(data.payload.quantity);
+                    let newStatus = "partially_filled";
+                    if (newFill === parseFloat(orderData?.quantity?.toString()!)) {
+                        newStatus = "filled";
+                    }
                     console.log(orderData)
+                    console.log(newStatus, newFill);
+                    await db.update(order).set({
+                        executedQuantity: newFill.toString(),
+                        status: newStatus as "open" | "partially_filled" | "filled" | "cancelled"
+                    }).where(and(eq(order.userId, data.payload.userId), eq(order.clientOrderId, data.payload.clientOrderId)));
+                    break;
             }
         }
     }
